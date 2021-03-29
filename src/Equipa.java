e63ae113ac6f9e42ad9ddfaa15d7254d5b3e1c97
AureoBenedito <50468427+AureoBenedito@users.noreply.github.com>
2021-03-29 15:37:48 +0100

    Update Equipa.java

--- src/Equipa.java
+++ src/Equipa.java
@@ -1,44 +1,45 @@
 import java.util.HashMap;
-import java.util.scanner;
+import java.util.Scanner;
 import java.io.Serializable;
+import java.util.List;
 
 public abstract class Equipa implements Comparable<Equipa>, Serializable{
-	private HashMap<String, List<Jogador>> nome;
-}
+    private HashMap<String, List<Jogador>> nome;
 
 public Equipa(){
-	this.nome = new HashMap();
+    this.nome = new HashMap();
 }
 
 public Equipa (HashMap<String, List<Jogador>> nome){
-	this.nome=nome;
+    this.nome=nome;
 }
 
 public Equipa (Equipa e){
-	this.nome = e.getNome();
+    this.nome = e.getNome();
 }
 
 public HashMap getNome(){
-	return this.nome;
+    return this.nome;
 }
 
-public void setNome(HashMap<String, List<Jogador>> name){
-	this.name=nome;
-}
+   public void setNome(HashMap<String, List<Jogador>> name){
+    this.nome=nome;
+   }
 
-public toString(){
-	return "Nome da Equipa: "+ this.nome;
-}
+    public String toString(){
+    return "Nome da Equipa: "+ this.nome;
+    }
 
 public abstract Equipa clone();
 
-public boolean equals(Object o){
-	if (this==o)
-		return true;
-	if((o==null)||(this.getClass()!=o.getClass()))
-		return false;
-	Equipa e = (Equipa) o;
-	return (this.nome) == (e.getNome());
+    public boolean equals(Object o){
+    if (this==o)
+        return true;
+    if((o==null)||(this.getClass()!=o.getClass()))
+        return false;
+    Equipa e = (Equipa) o;
+    return (this.nome) == (e.getNome());
+}
 }
 
 
