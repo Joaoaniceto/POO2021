881e9e01389aa3b93a5a65eb324a4b0fda009b11
João Aniceto A80292 <54753771+Joaoaniceto@users.noreply.github.com>
2021-04-01 15:31:20 +0100

    Add files via upload
    update equipa

--- src/Equipa.java
+++ src/Equipa.java
@@ -1,47 +1,70 @@
 import java.util.HashMap;
 import java.util.Scanner;
 import java.io.Serializable;
-import java.util.List;
+import java.util.TreeMap;
 
 public abstract class Equipa implements Comparable<Equipa>, Serializable{
-    private HashMap<String, List<Jogador>> nome;
+    private String name;
+    private HashMap<Integer, Jogador> jogadores;
 
 public Equipa(){
-    this.nome = new HashMap();
+    this.name = "";
+    this.jogadores = new HashMap<Integer, Jogador>();
 }
 
-public Equipa (HashMap<String, List<Jogador>> nome){
-    this.nome=nome;
+public Equipa (String name,HashMap<Integer, Jogador> jogadores){
+    this.name=name;
+    this.jogadores = jogadores;
 }
 
 public Equipa (Equipa e){
-    this.nome = e.getNome();
+    this.name = e.getName();
+    this.jogadores = e.getJogadores();
 }
 
-public HashMap getNome(){
-    return this.nome;
+public String getName(){
+    return this.name;
 }
 
-   public void setNome(HashMap<String, List<Jogador>> name){
-    this.nome=nome;
-   }
-
-    public String toString(){
-    return "Nome da Equipa: "+ this.nome;
+public HashMap<Integer, Jogador> getJogadores() {
+        return this.jogadores;
     }
 
+public void setNome(String name){
+    this.name=name;
+}
+   
+public void setjogadores(HashMap<Integer, Jogador> jogadores){
+    this.jogadores=jogadores;
+}
 public abstract Equipa clone();
-
-    public boolean equals(Object o){
+public boolean equals(Object o){
     if (this==o)
         return true;
     if((o==null)||(this.getClass()!=o.getClass()))
         return false;
     Equipa e = (Equipa) o;
-    return (this.nome) == (e.getNome());
+    return (this.name) == (e.getName());
 }
+public void addJogador(Jogador o){
+   this.jogadores.put(o.getNum(),o.clone());
+}
+public void removeJogador(Jogador o){
+    if (this.jogadores.entrySet().stream().anyMatch(b -> b.equals(o))){
+    this.jogadores.remove(o.getNum());
+}
+}
+public void transferenciaJogador(Equipa a,Jogador o){
+   if (this.jogadores.entrySet().stream().anyMatch(b -> b.equals(o))){
+    removeJogador(o);
+    a.addJogador(o);
 }
 
+
+}
+
+
+}
 
 
 
