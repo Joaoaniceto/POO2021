464ee0e7c7daad385b45451c999c48255c46e8c4
AureoBenedito <50468427+AureoBenedito@users.noreply.github.com>
2021-04-15 18:19:19 +0100

    Add "drible"

--- src/Avançado.java
+++ src/Avançado.java
@@ -4,18 +4,24 @@ public class Avançado extends Jogador {
     //construtor por omissão
     public Avançado(){
         super();
+        private int drible;
     }
 
     //construtor parametrizado
     public Avançado(int numero, String nome, boolean titular, ArrayList<Equipa> h, int vel, int res,
-                    int des, int imp, int cab, int rem, int passe){
+                    int des, int imp, int cab, int rem, int passe,  int drible){
         super(numero,nome,titular,h,vel,res,des,imp,cab,rem,passe);
+        this.drible=drible;
     }
 
     //construtor por objeto
     public Avançado(Avançado l){
         super(l);
+        this.corte= l.getDrible();
     }
+    
+    public int getDrible(){ return this.drible:}
+    public void setCorte(int d){ this.drible=d;}
 
     @Override
     public int compareTo(Jogador o) {
@@ -27,6 +33,6 @@ public class Avançado extends Jogador {
     }
 
     public double gethabilidade(){
-        return super.getVel()*0.2 + super.getRes()*0.05+ super.getDes()*0.05 + super.getImp()*0.1 + super.getCab()*0.2 + super.getRem()*0.3 + super.getPasse()*0.1;
+        return super.getVel()*0.2 + super.getRes()*0.05+ super.getDes()*0.05 + super.getImp()*0.1 + super.getCab()*0.1 + super.getRem()*0.2 + super.getPasse()*0.1+ this.drible*0.2;
     }
 }
