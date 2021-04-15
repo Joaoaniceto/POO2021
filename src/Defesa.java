7175377d12b8007d97e425dbb6588d923aee58bd
AureoBenedito <50468427+AureoBenedito@users.noreply.github.com>
2021-04-15 18:10:55 +0100

    Adicionei a variavel "corte"

--- src/Defesa.java
+++ src/Defesa.java
@@ -4,18 +4,23 @@ public class Defesa extends Jogador {
     //construtor por omissão
     public Defesa(){
         super();
+        private int corte;
     }
 
     //construtor parametrizado
     public Defesa(int numero, String nome, boolean titular, ArrayList<Equipa> h, int vel, int res,
-                  int des, int imp, int cab, int rem, int passe){
+                  int des, int imp, int cab, int rem, int passe, int corte){
         super(numero,nome,titular,h,vel,res,des,imp,cab,rem,passe);
     }
 
     //construtor por objeto
     public Defesa(Defesa l){
         super(l);
+        this.corte=l.getCorte;
     }
+    
+    public int getCorte(){ return this.corte;}
+    public void setCorte(int c){ this.corte=c;}
 
     @Override
     public int compareTo(Jogador o) {
@@ -27,7 +32,7 @@ public class Defesa extends Jogador {
     }
 
     public double gethabilidade(){
-     return super.getVel()*0.1 + super.getRes()*0.2+ super.getDes()*0.2 + super.getImp()*0.1 + super.getCab()*0.2 + super.getRem()*0.1 + super.getPasse()*0.1;
+     return super.getVel()*0.05 + super.getRes()*0.2+ super.getDes()*0.1 + super.getImp()*0.1 + super.getCab()*0.2 + super.getRem()*0.1 + super.getPasse()*0.1+ this.corte*0.35;
       }
 
 }
